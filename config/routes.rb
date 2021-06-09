17e0fc5b54b7bbd189a799bdc976dfe4952c976e
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-06-09 16:27:58 -0300

    added update edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
     root 'pages#home'
     get 'about', to: 'pages#about'
-    resources :articles, only: [:show, :index, :new, :create]
+    resources :articles
   end
 end
